436c9eff58d1d092f00dd5fbe06c1d0dc9762406
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2022-04-30 21:32:55 +0200

    Refactor helper for feed to get last 10 posts

--- lib/sgoettschkes/feed_helpers.ex
+++ lib/sgoettschkes/feed_helpers.ex
@@ -9,15 +9,8 @@ defmodule Sgoettschkes.FeedHelpers do
 
   def get_feed_url(), do: Application.get_env(:sgoettschkes, :site_url) <> "feed.xml"
 
-  def get_posts() do
-    Sgoettschkes.Compiler.Post.get()
-    |> Enum.sort_by(
-      fn elem ->
-        elem.metadata.date
-        |> Timex.parse!("%Y-%m-%d %H:%M:%S %Z", :strftime)
-      end,
-      {:desc, Date}
-    )
+  def get_feed_posts() do
+    Sgoettschkes.IndexHelpers.get_all_posts()
     |> Enum.take(10)
   end
 
