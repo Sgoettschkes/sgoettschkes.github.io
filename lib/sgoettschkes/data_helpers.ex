ceee3541e357fb644fa919636d0c0887e17dd8ec
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2022-04-30 20:11:39 +0200

    Refactor helpers

--- lib/sgoettschkes/data_helpers.ex
+++ lib/sgoettschkes/data_helpers.ex
@@ -1,39 +1,32 @@
 defmodule Sgoettschkes.DataHelpers do
-  def about_seenon() do
-    read_yaml("priv/site/_data/about_seenon.yaml")
-  end
+  def about_seenon(), do: read_yaml("about_seenon.yaml")
 
-  def about_projects() do
-    read_yaml("priv/site/_data/about_projects.yaml")
-  end
+  def about_projects(), do: read_yaml("about_projects.yaml")
 
   def about_recent_talks() do
-    read_yaml("priv/site/_data/about_talks.yaml")
+    "about_talks.yaml"
+    |> read_yaml()
     |> Map.get("recent")
   end
 
   def about_upcoming_talks() do
-    read_yaml("priv/site/_data/about_talks.yaml")
+    "about_talks.yaml"
+    |> read_yaml()
     |> Map.get("upcoming")
   end
 
-  def about_workshops() do
-    read_yaml("priv/site/_data/about_workshops.yaml")
-  end
+  def about_workshops(), do: read_yaml("about_workshops.yaml")
 
-  def about_publications() do
-    read_yaml("priv/site/_data/about_publications.yaml")
-  end
+  def about_publications(), do: read_yaml("about_publications.yaml")
 
   def about_contributions() do
-    read_yaml("priv/site/_data/about_contributions.yaml")
+    "about_contributions.yaml"
+    |> read_yaml()
     |> Enum.sort(&(String.downcase(&1["repo"]) <= String.downcase(&2["repo"])))
     |> Enum.sort(&(String.downcase(&1["org"]) <= String.downcase(&2["org"])))
   end
 
-  def now_doing() do
-    read_yaml("priv/site/_data/now_doing.yaml")
-  end
+  def now_doing(), do: read_yaml("now_doing.yaml")
 
   def now_last_updated() do
     File.cwd!()
@@ -45,9 +38,10 @@ defmodule Sgoettschkes.DataHelpers do
     |> Timex.format!("%B %Y", :strftime)
   end
 
-  defp read_yaml(path) do
+  defp read_yaml(filename) do
     File.cwd!()
-    |> Path.join(path)
+    |> Path.join("priv/site/_data")
+    |> Path.join(filename)
     |> YamlElixir.read_from_file!(atoms: true)
   end
 end
--- lib/sgoettschkes/robot_helpers.ex
+++ lib/sgoettschkes/robot_helpers.ex
@@ -1,7 +1,3 @@
 defmodule Sgoettschkes.RobotHelpers do
-  def get_sitemap_url() do
-    site_url = Application.get_env(:sgoettschkes, :site_url)
-
-    site_url <> "sitemap.xml"
-  end
+  def get_sitemap_url(), do: Application.get_env(:sgoettschkes, :site_url) <> "sitemap.xml"
 end
