e547eb8b226bebe802e9303ea6e688bf4ea24b0c
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-09-25 06:54:29 +0200

    Update elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sgoettschkes.MixProject do
     [
       app: :sgoettschkes,
       version: "0.1.0",
-      elixir: "~> 1.14.5",
+      elixir: "~> 1.15.6",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
