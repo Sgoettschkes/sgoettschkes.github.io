b528944576593f429b8da6e2e1cfc7136129eec0
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2024-06-08 08:53:36 +0200

    Try fallback to elixir 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sgoettschkes.MixProject do
     [
       app: :sgoettschkes,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
